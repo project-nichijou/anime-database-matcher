c3712a8f6be7db85bba1d792dc97347e8f609342
JeffersonQin <1247006353@qq.com>
2021-07-24 01:00:11 +0800

    [Add] columns support

--- database/nichijou_database.py
+++ database/nichijou_database.py
@@ -1,3 +1,5 @@
+import traceback
+from utils.logger import format_log
 from database.database import CommonDatabase
 
 
@@ -5,3 +7,47 @@ class NichijouDatabase(CommonDatabase):
 
 	def __init__(self):
 		super().__init__('nichijou')
+
+
+	def check_source_column(self, source):
+		'''
+		check whether source column exists
+		'''
+		try:
+			cursor = self.get_cursor(dictionary=True)
+			cursor.execute('DESC `anime`')
+			res = cursor.fetchall()
+			for item in res:
+				if item['Field'] == source:
+					return True
+			return False
+		except Exception as e:
+			self.log(format_log(
+				info='exception caught when checking source column.',
+				exception=e,
+				traceback=traceback.format_exc(),
+				values={
+					'source': source
+				}
+			))
+			return False
+
+
+	def add_source_column(self, source):
+		'''
+		create source column if not exist
+		'''
+		try:
+			if not self.check_source_column(source=source):
+				cursor = self.get_cursor()
+				cursor.execute(f'ALTER TABLE `anime` ADD COLUMN `{source}` INT UNSIGNED')
+				cursor.close()
+		except Exception as e:
+			self.log(format_log(
+				info='exception caught when adding source column.',
+				exception=e,
+				traceback=traceback.format_exc(),
+				values={
+					'source': source
+				}
+			))
